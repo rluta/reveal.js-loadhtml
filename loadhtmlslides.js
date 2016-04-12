6701a926b3c05b7a585590c9b0c46d434a902747
Raphael Luta <raphael.luta@aptiwan.com>
2016-04-12 15:19:46 +0200

    revert to straight attachment of js script in DOM

--- loadhtmlslides.js
+++ loadhtmlslides.js
@@ -23,6 +23,16 @@
                                 xhr.status === 0 // file protocol yields status code 0 (useful for local debug, mobile applications etc.)
                             ) {
                                 section.innerHTML = xhr.responseText;
+                                if (section.getAttribute('data-js') !== null) {
+                                    // make sure we set a statename to the section if not present
+                                    if (section.getAttribute('data-state') === null) {
+                                        section.setAttribute('data-state',id);
+                                    }
+                                    var jsurl = section.getAttribute('data-js');
+                                    var script = document.createElement('script')
+                                    script.src = jsurl;
+                                    section.appendChild(script)
+                                }
                             } else {
                                 section.outerHTML = '<section data-state="alert">ERROR: The attempt to fetch ' + url + ' failed with the HTTP status ' + xhr.status + '. Check your browser\'s JavaScript console for more details.</p></section>';
                             }
@@ -38,41 +48,6 @@
                     alert('Failed to get file' + url + '.' + e);
                 }
             }
-
-            if (section.getAttribute('data-js') !== null) {
-                // make sure we set a statename to the section if not present
-                if (section.getAttribute('data-state') === null) {
-                    section.setAttribute('data-state',id);
-                }
-
-                var jsxhr = new XMLHttpRequest(),
-                    jsurl = section.getAttribute('data-js'),
-                    jscb = function () {
-                        if (jsxhr.readyState === 4) {
-                            if (
-                                (jsxhr.status >= 200 && jsxhr.status < 300) ||
-                                jsxhr.status === 0 // file protocol yields status code 0 (useful for local debug, mobile applications etc.)
-                            ) {
-                                var script = document.createElement('script')
-                                script.innerText = "Reveal.addEventListener('"+id+"',function () {\n"+
-                                    jsxhr.responseText+"\n},false);";
-                                section.appendChild(script)
-                            } else {
-                                console.log("Failed to load script "+jsurl);
-                            }
-                        }
-                    };
-
-                jsxhr.onreadystatechange = jscb;
-
-                jsxhr.open('GET', jsurl, false);
-                try {
-                    jsxhr.send();
-                } catch (e) {
-                    console.error('Failed to get file' + jsurl + '.' + e);
-                }
-            }
-
         }
     };
 
